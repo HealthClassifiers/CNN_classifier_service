755ebcf0ddf2d1975d9ad7217bef755a6d49c0be
Casper Hansen <caspha17@gmail.com>
2021-11-25 08:13:55 +0100

    Added socket connection

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from logging import error
 import numpy as np
-
+from PIL import Image
 from flask import Flask, request, make_response
 from finished_classifier import finished_classifier
 from user_data_persistance import PersistanceModule
-
+from flask_socketio import SocketIO, emit
+from io import BytesIO  
+from flask_cors import CORS, cross_origin
+import socketio
+from pymongo import MongoClient
 error_messages = {'NO_IMAGE_PROVIDED': 'You have not provided an image in correct format to be classified.'}
 
+
 app = Flask("ISS_Classifier")
+cors = CORS(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, logger=True , cors_allowed_origins='*')
 
 @app.route('/classify', methods=['POST'])
 def classify_image():
-    file = request.files['image'].read()
+    file = request.files['image']
     if file != None:
-        # npimg = np.fromstring(file, np.uint8)
         classifier_service = finished_classifier()
-        identified_class = classifier_service.classify_image(npimg)
+        identified_class = classifier_service.classify_image(file)
         return identified_class
     else:
         return error_messages(error_messages['NO_IMAGE_PROVIDED'])
@@ -25,9 +32,6 @@ def test():
     db = PersistanceModule()
     
     classifier = finished_classifier(save_on_classification=False)
-
-    print(classifier)
-
     result = classifier.classify_image('./test_img.jpg')
 
     # db.show_user_data()
@@ -35,5 +39,30 @@ def test():
     response.mimetype = "test/plain"
     return response
 
+@app.route('/doctor', methods=['POST'])
+def ping():
+    file = request.files['image']
+    img = Image.open(file)
+    img_byte_arr = BytesIO()
+    img.save(img_byte_arr, format='jpeg')
+    socketio.emit('result' , img_byte_arr.getvalue() )
+    # db.show_user_data()
+    response = make_response('Ok', 200)
+    response.mimetype = "test/plain"
+    return response
+
+@app.route('/medicalRecords', methods=['POST'])
+def addMedicalRecord():
+    print('received message: ' )
+    client = MongoClient("mongodb://mongo:27017")
+    db = client.medicalrecords
+    db.medicalrecords.insert_one()
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    #app.run(host="0.0.0.0", debug=True)
+     socketio.run(app, debug=True)
--- user_data_persistance.py
+++ user_data_persistance.py
@@ -4,9 +4,11 @@ class PersistanceModule:
     def __init__(self):
         self.client = MongoClient("mongodb://mongo:27017")
         self.db = self.client.user_data
+    
 
     def insert_user_data(self, img):
-        self.db.insert_one(img)
+        #self.db.insert_one(img)
+        print(img)
 
     def show_user_data(self):
         images = self.db._list_collections()
